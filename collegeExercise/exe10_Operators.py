1dd683bf1c1dda7c73ef6446964c6b364aaa763b
Rushik Maniar <rushikmaniar107@gmail.com>
2018-12-04 12:50:33 +0530

    exe8,9 done

---
+++ collegeExercise/exe10_Operators.py
@@ -0,0 +1,50 @@
+'''
+    Use of Operators
+'''
+print('\n\nAssisgnment Operator')
+a = 2
+print('a : ',a)
+
+a += 2
+print('a += 2',a)
+
+a -= 2
+print('a -= 2',a)
+
+a *= 2
+print('a *= 2',a)
+
+a /= 2
+print('a /= 2',a)
+
+
+print('\n\n\nRelatonal Operator')
+a = 10
+b = 5
+
+print('A : ',a)
+print('B : ',b)
+
+if(a < b):
+    print('\nA < B')
+if(a <=b ):
+    print('A <= B')
+if(a > b ):
+    print('A > B')
+if(a >= b ):
+    print('A >= B')
+if(a == b ):
+    print('A == B')
+if(a != b ):
+    print('A != B')
+
+print('\n\n\nMembership Operator')
+list = (10,2,6,3)
+
+if(2 in list):
+    print('2 in list ')
+if(2 not in list):
+    print('2 not in list ')
+
+
+
---
+++ collegeExercise/functionlist.py
@@ -0,0 +1,17 @@
+'''
+    list of function
+'''
+a = 'Rushik'
+print('isupper() : ',a.isupper())
+print('isspace() :',a.isspace())
+print('Len() : ',len(a))
+print('lower()',a.lower())
+print('upper()',a.upper())
+print('swapcase()',a.swapcase())
+print('lstrip() :',a.lstrip())
+print('count() : ',a.count('Rus'))
+print('find() :',a.find('ru'))
+print('isalnum() :',a.isalnum())
+print('isalpha() :',a.isalpha())
+print('isdigit() :',a.isdigit())
+print('islower() :',a.islower())
